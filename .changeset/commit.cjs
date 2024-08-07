f606337ed6b87a0e2a32fccae48a4e6215a83710
Jake Holland <contact@hollandjake.com>
2024-08-07 09:38:14 +0100

    fix(changeset): trying to get release working

--- .changeset/commit.cjs
+++ .changeset/commit.cjs
@@ -1,7 +1,6 @@
 /**
  *
  * @typedef {import('@changesets/types').Changeset} Changeset
- * @typedef {import('@changesets/types').VersionType} VersionType
  * @typedef {import('@changesets/types').ReleasePlan} ReleasePlan
  */
 module.exports = {
@@ -11,21 +10,15 @@ module.exports = {
    * @returns {Promise<string>}
    */
   getAddMessage: async changeset => {
-    /** @type {VersionType[]} */
-    const RANK_MAP = ['none', 'patch', 'minor', 'major'];
-    const ranking = RANK_MAP[Math.max(
-      0,
-      ...changeset.releases.map(r => RANK_MAP.indexOf(r.type))
-    )];
-    return `${ranking}(changeset): ${changeset.summary}`;
+    return `docs(changeset): ${changeset.summary}`;
   },
 
-  /**
-   *
-   * @param {ReleasePlan} releasePlan
-   * @returns {Promise<void>}
-   */
-  getVersionMessage: async releasePlan => {
-    return `Release ${releasePlan.releases[0].newVersion}`;
-  }
+  // /**
+  //  *
+  //  * @param {ReleasePlan} releasePlan
+  //  * @returns {Promise<void>}
+  //  */
+  // getVersionMessage: async releasePlan => {
+  //   return `Release ${releasePlan.releases[0].newVersion}`;
+  // }
 };
